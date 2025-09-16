2d5fabc67935abe9ec81b5a0fb8f0c77db9bddec
Luan <luan.valentino78@gmail.com>
2025-09-16 15:36:19 -0300

    commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-from typing import Callable
+from functools import wraps
+from typing import Callable, Any
+
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_storage = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        key = (args, tuple(kwargs.items()))
+        if key in cache_storage:
+            print("Getting from cache")
+            return cache_storage[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_storage[key] = result
+            return result
+
+
+    return wrapper
+
